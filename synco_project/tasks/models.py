bda60966a4377fa8d3d21f903fab27c604ba358d
AniaNiedzialek <ania.niedzialek@sjsu.edu>
2025-09-05 12:56:07 -0700

    updating the database

--- synco_project/tasks/models.py
+++ synco_project/tasks/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.db import models
 class Task(models.Model):
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+    # tells django that each task is linked ot a user, for now avoiding issues
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return self.text
