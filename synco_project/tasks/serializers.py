070cd3c2eaf8eecd5be55e0bc315d98ef3de0cd8
AniaNiedzialek <ania.niedzialek@sjsu.edu>
2025-09-05 12:58:14 -0700

    adding django REST framework for serializers.py - it tells the serializer to get the username from the related User object - making the API response more readable

--- synco_project/tasks/serializers.py
+++ synco_project/tasks/serializers.py
@@ -2,8 +2,12 @@
 
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
+
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Task
         fields = ['id', 'text', 'created_at']
+        read_only_fields = ['id', 'created_at', 'user']
         
